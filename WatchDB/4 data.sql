9d3988acab398b4998d0ac90db6d45ee29703506
AvigailGellis <agellis@cpucodeschool.com>
2025-09-30 02:14:23 -0400

    fixes

--- WatchDB/4 data.sql
+++ WatchDB/4 data.sql
@@ -8,6 +8,5 @@ and o.[Status] = 'sold'
 --2: The Watch Collection(phoenix) needs to be restored, but they complained that watches with brown leather bands don't sell, and just take up space on his limited display, I need a list of all watches in stock that DON'T have BROWN leather bands.
 select *
 from [Order] o 
-where o.StoreName like '%phoenix%'
-and o.[Status] = 'in stock'
+where o.[Status] = 'in stock'
 and o.WatchDesc like '%brown leather%'
